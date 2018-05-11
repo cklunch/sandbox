0c701584b833b6b766eca82a493558715bde6dab
cklunch <clunch@neoninc.org>
2018-05-11 10:47:03 -0600

    release schedule code

--- dp_availability.R
+++ dp_availability.R
@@ -1,7 +1,8 @@
 library(httr)
 library(jsonlite)
-library(dplyr)
-library(googlesheets)
+library(plyr)
+
+options(stringsAsFactors=F)
 
 # get availability list
 req <- GET("http://data.neonscience.org/api/v0/products")
@@ -25,6 +26,39 @@ for(i in 1:nrow(current)) {
 }
 
 # next up: include NAs for sites that won't ever have product
+mat <- read.delim("~/sandbox/Master_product_siteMatrix.csv", sep=",", skip=5)
+mat <- mat[1:180,1:85]
+
+# precip wrangling
+precip <- apply(t(mat[which(mat$Identification.Code=="NEON.DOM.SITE.DP1.00006.001"),5:85]), 
+                2, as.numeric)
+precip <- apply(precip, 1, sum)
+mat <- mat[-which(mat$Identification.Code=="NEON.DOM.SITE.DP1.00006.001"),]
+mat <- rbind(mat, c("Precipitation", "NEON.DOM.SITE.DP1.00006.001", "TIS", "", precip))
+
+for(i in colnames(mat)[5:85]) {
+  for(j in mat$Identification.Code) {
+    mat.val <- mat[which(mat$Identification.Code==j), which(colnames(mat)==i)]
+    if(mat.val > 0) {
+      next
+    } else {
+      id <- substring(j, 15, 27)
+      cur.val <- current[which(current$DPID==id), which(colnames(current)==i)]
+      if(cur.val==0) {
+        current[which(current$DPID==id), which(colnames(current)==i)] <- NA
+      } else {
+        print(paste("Data available for ", id, " at ", i, " but not in matrix", sep=""))
+      }
+    }
+  }
+}
 
 # for latency: import transition wait times google sheet
+tran <- read.delim("~/sandbox/Transition_wait_times.csv", sep=",")
+
+# max transition time by product
+tranByProd <- ddply(tran, "DPName", summarise, latency=max(Transition.wait.time, na.rm=T))
+tranByProd$latency[which(tranByProd$latency==-Inf)] <- NA
+
+
 
