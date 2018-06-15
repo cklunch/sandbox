7d26f9442b7239b8dc9c68ff07596f9d6a5a0e07
cklunch <clunch@neoninc.org>
2018-06-15 15:51:16 -0600

    continuing ec access

---
+++ EC_HDF/stackEC.R
@@ -0,0 +1,38 @@
+##############################################################################################
+#' @title Sequentially extract elements from NEON EC HDF files and merge into one table
+
+#' @author
+#' Claire Lunch \email{clunch@battelleecology.org}
+
+#' @description
+#' Given a list of NEON EC files, pass to tabular conversion function, then merge into one table.
+
+#' @param filepath The folder containing the H5 files [character]
+#' @param site The NEON site of the data [character]
+#' @param level The level of data to extract; one of dp01, dp02, dp03, dp04, dp0p [character]
+#' @param var The variable set to extract, e.g. co2Turb [character]
+#' @param avg The averaging interval to extract, in minutes [numeric]
+
+#' @return A data frame of the available data for the specified variable.
+
+#' @references
+#' License: GNU AFFERO GENERAL PUBLIC LICENSE Version 3, 19 November 2007
+
+# Changelog and author contributions / copyrights
+#   2018-06-15 (Claire Lunch): Original creation
+##############################################################################################
+
+stackEC <- function(filepath, site, level, var, avg) {
+  
+  # get list of files
+  files <- list.files(filepath, recursive=T)
+  files <- files[grep(".h5", files)]
+  
+  # pass to flattenH5EC() and merge
+  # need to allocate space up front
+  for(i in files) {
+    i.df <- flattenH5EC(paste(filepath, files[i], sep="/"), site=site, level=level, var=var, avg=avg)
+    
+  }
+  
+}
