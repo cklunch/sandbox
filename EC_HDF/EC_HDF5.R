da31a3dd63385cd5d66f3e4488f5d1a572e36fb9
cklunch <clunch@neoninc.org>
2018-06-22 10:39:32 -0600

    cleaning up examples

--- EC_HDF/EC_HDF5.R
+++ EC_HDF/EC_HDF5.R
@@ -1,41 +1,20 @@
+# install packages
 source("http://bioconductor.org/biocLite.R")
 biocLite("rhdf5")
+devtools::install_github("NEONScience/NEON-utilities/neonUtilities")
 
 #
 library(rhdf5)
 library(neonUtilities)
 library(plyr)
-library(tidyr)
 
 # try it out
 zipsByProduct(dpID="DP4.00200.001", site="BART", package="expanded", savepath="/Users/clunch/Desktop")
-zipsByProduct(dpID="DP4.00200.001", site="CPER", package="expanded", savepath="/Users/clunch/Desktop")
 
-# manually unzipped before moving forward
+stack.co2 <- stackEC(filepath="/Users/clunch/Desktop/filesToStack00200BART",
+                      site="BART", level="dp04", var="fluxCo2", avg=NA)
 
-stack.flux <- stackEC(filepath="/Users/clunch/Desktop/filesToStack00200",
-                      site="CPER", level="dp04", var="fluxCo2", avg=NA)
+stack.h2o.dp02 <- stackEC(filepath="/Users/clunch/Desktop/filesToStack00200BART",
+                     site="BART", level="dp02", var="h2oStor", avg="30")
 
-stack.h2o <- stackEC(filepath="/Users/clunch/Desktop/filesToStack00200",
-                      site="BART", level="dp04", var="fluxH2o", avg=NA)
-
-
-turb30 <- flattenH5EC(filepath="/Users/clunch/Desktop/filesToStack00200/NEON.D01.BART.DP4.00200.001.2017-09.expanded.20180424T184516Z/NEON.D01.BART.DP4.00200.001.nsae.2017-09-29.expanded.h5",
-            site="BART", level="dp01", var="co2Turb", avg=30)
-
-h2o.l2 <- flattenH5EC(filepath="/Users/clunch/Desktop/filesToStack00200/NEON.D01.BART.DP4.00200.001.2017-09.expanded.20180424T184516Z/NEON.D01.BART.DP4.00200.001.nsae.2017-09-29.expanded.h5",
-                      site="BART", level="dp02", var="h2oStor", avg=30)
-
-# no data in this one
-shf30 <- flattenH5EC(filepath="/Users/clunch/Desktop/filesToStack00200/NEON.D01.BART.DP4.00200.001.2017-09.expanded.20180424T184516Z/NEON.D01.BART.DP4.00200.001.nsae.2017-09-29.expanded.h5",
-                   site="BART", level="dp01", var="fluxHeatSoil", avg=30)
-
-isoC <- flattenH5EC(filepath="/Users/clunch/Desktop/filesToStack00200/NEON.D01.BART.DP4.00200.001.2017-09.expanded.20180424T184516Z/NEON.D01.BART.DP4.00200.001.nsae.2017-09-29.expanded.h5",
-                    site="BART", level="dp01", var="isoCo2", avg=9)
-
-nee <- flattenH5EC(filepath="/Users/clunch/Desktop/filesToStack00200/NEON.D01.BART.DP4.00200.001.2017-09.expanded.20180424T184516Z/NEON.D01.BART.DP4.00200.001.nsae.2017-09-29.expanded.h5",
-                   site="BART", level="dp04", var="fluxCo2", avg=NA)
-
-temp.l3 <- flattenH5EC(filepath="/Users/clunch/Desktop/filesToStack00200/NEON.D01.BART.DP4.00200.001.2017-09.expanded.20180424T184516Z/NEON.D01.BART.DP4.00200.001.nsae.2017-09-29.expanded.h5",
-                       site="BART", level="dp03", var="tempStor", avg=NA)
 
