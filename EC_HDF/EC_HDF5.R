3403dfbfd74f0b169e5bf629ad57a49112af07a3
cklunch <clunch@neoninc.org>
2018-06-22 10:47:56 -0600

    clarifying examples

--- EC_HDF/EC_HDF5.R
+++ EC_HDF/EC_HDF5.R
@@ -3,14 +3,16 @@ source("http://bioconductor.org/biocLite.R")
 biocLite("rhdf5")
 devtools::install_github("NEONScience/NEON-utilities/neonUtilities")
 
-#
+# load packages
 library(rhdf5)
 library(neonUtilities)
 library(plyr)
 
-# try it out
+# download some data
 zipsByProduct(dpID="DP4.00200.001", site="BART", package="expanded", savepath="/Users/clunch/Desktop")
 
+# try out the extracting functions
+# (have to source stackEC(), flattenH5EC(), and H5ECtoDF() for this to work)
 stack.co2 <- stackEC(filepath="/Users/clunch/Desktop/filesToStack00200BART",
                       site="BART", level="dp04", var="fluxCo2", avg=NA)
 
