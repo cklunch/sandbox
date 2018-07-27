e167b2f3200029230a1aca0f14040a6de21681a7
cklunch <clunch@neoninc.org>
2018-07-26 18:02:35 -0600

    esa prepping

--- esa_prep.R
+++ esa_prep.R
@@ -3,6 +3,8 @@ library(neonUtilities)
 library(geoNEON)
 library(sp)
 library(rgdal)
+options(stringsAsFactors = F)
+
 
 # NDNI
 ndni <- raster("/Users/clunch/Downloads/2017 4/FullSite/D06/2017_KONZ_2/L3/Spectrometer/VegIndices/NEON_D06_KONZ_DP3_708000_4327000_NDNI.tif")
@@ -16,7 +18,7 @@ extent(ndni)
 
 
 # CHM
-chm <- raster("/Users/clunch/Desktop/2017 UNDE/FullSite/D05/2017_UNDE_2/L3/DiscreteLidar/CanopyHeightModelGtif/NEON_D05_UNDE_DP3_309000_5120000_CHM.tif")
+chm <- raster("/Users/clunch/Desktop/2016/FullSite/D05/2016_UNDE_1/L3/DiscreteLidar/CanopyHeightModelGtif/2016_UNDE_1_304000_5122000_pit_free_CHM.tif")
 summary(chm)
 plot(chm, col=topo.colors(6))
 contour(chm)
@@ -38,9 +40,24 @@ View(fol)
 zipsByProduct(dpID="DP1.10098.001", site="UNDE", package="expanded", 
               savepath="/Users/clunch/Desktop/struct")
 stackByTable("/Users/clunch/Desktop/struct/filesToStack10098/", folder=T)
-veg <- read.delim("/Users/clunch/Desktop/struct/filesToStack10098/stackedFiles/vst_mappingandtagging.csv",
+vegmap <- read.delim("/Users/clunch/Desktop/struct/filesToStack10098/stackedFiles/vst_mappingandtagging.csv",
                   sep=",")
-View(veg)
-# mappingandtagging has distance & azimuth, apparentindividual has height
+View(vegmap)
+# mappingandtagging has distance & azimuth, apparentindividual has height,
+# perplotperyear has coordinates
+vegind <- read.delim("/Users/clunch/Desktop/struct/filesToStack10098/stackedFiles/vst_apparentindividual.csv",
+                     sep=",")
+View(vegind)
+vegp <- read.delim("/Users/clunch/Desktop/struct/filesToStack10098/stackedFiles/vst_perplotperyear.csv",
+                     sep=",")
+View(vegp)
+# vegmap <- geoCERT::def.extr.geo.os(vegmap)
+# symbols(vegmap$api.easting, vegmap$api.northing, 
+#         circles=rep(20, nrow(vegmap)), inches=F)
 
+vegind <- geoCERT::def.extr.geo.os(vegind)
+symbols(vegind$api.easting, vegind$api.northing, 
+        circles=rep(20, nrow(vegind)), inches=F, add=T)
+# need to merge individuals with mapping & tagging to get distance & azimuth,
+# then make calc function
 
