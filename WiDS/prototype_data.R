cebee48f7e7ba207589362321bce69ef23ed17ea
Claire Lunch <clunch@battelleecology.org>
2018-08-27 16:15:46 -0600

    WiDS examples

--- WiDS/prototype_data.R
+++ WiDS/prototype_data.R
@@ -1,6 +1,7 @@
 # prototypes for WiDS
 
 library(neonUtilities)
+options(stringsAsFactors=F)
 
 zipsByProduct("DP1.10093.001", site="TALL", package="expanded", 
                       savepath="/Users/clunch/sandbox/WiDS/")
@@ -40,4 +41,34 @@ tick <- merge(tickColl, tickPathSub, by=c("subsampleID"), all=F)
 write.table(tick[1:500,], file="/Users/clunch/sandbox/WiDS/tick_example.csv", 
             sep=",", row.names=F)
 
+
+## other options
+zipsByProduct("DP1.20033.001", site="WALK", package="basic", 
+              savepath="/Users/clunch/sandbox/WiDS/")
+stackByTable("/Users/clunch/sandbox/WiDS/filesToStack20033", folder=T)
+
+nsw <- read.delim("/Users/clunch/sandbox/WiDS/filesToStack20033/stackedFiles/NSW_15_minute.csv",
+                  sep=",")
+# nope. ~1 million rows
+
+#zipsByProduct("DP1.10098.001", site="WREF", package="basic", 
+#              savepath="/Users/clunch/sandbox/WiDS/")
+
+vegmap <- read.delim("/Users/clunch/Desktop/ESA 2018 Wkshp Data/NEON_struct-woody-plant/stackedFiles/vst_mappingandtagging_geo.csv",
+                  sep=",")
+vegind <- read.delim("/Users/clunch/Desktop/ESA 2018 Wkshp Data/NEON_struct-woody-plant/stackedFiles/vst_apparentindividual.csv",
+                     sep=",")
+intersect(names(veg),names(vegind))
+veg <- merge(vegind, vegmap, by=c("individualID","namedLocation","domainID","siteID"))
+vegWREF <- veg[which(veg$siteID=="WREF"),]
+
+write.table(vegWREF, file="/Users/clunch/sandbox/WiDS/woody_veg_example.csv", 
+            sep=",", row.names=F)
+
+zipsByProduct("DP1.10055.001", site="ABBY", package="basic",
+              savepath="/Users/clunch/Desktop/")
+stackByTable("/Users/clunch/Desktop/filesToStack10055", folder=T)
+
+pheno <- read.delim("/Users/clunch/Desktop/filesToStack10055/stackedFiles/phe_statusintensity.csv",
+                    sep=",")
 
