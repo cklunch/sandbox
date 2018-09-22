5ee71a56f9a6f38eab9e021c60aa077ced97f4a4
cklunch <clunch@neoninc.org>
2018-09-21 18:29:19 -0600

    data availability graphs

--- data_availability/latencyGraphs.R
+++ data_availability/latencyGraphs.R
@@ -71,6 +71,20 @@ ggplot(summaryDat, aes(x = month, y = ((cumN/82)*100))) + geom_col() + ylab("% o
 ggsave('osDpTimeline.png')
 
 ##histograms of latency
+# all latencies
+datLat <- dat %>% filter(`Latency category` != "OT")
+ggplot(datLat, aes(`Transition wait time`)) + stat_bin(aes(y = (..count..)/sum(..count..)*100), binwidth = 30, color = "black", fill = "#CCCCCC") + theme_bw() + scale_y_continuous("% of OS data tables") + xlab("Latency (days since field collection)")
+
+# distribution of types
+ggplot(datLat, aes(reorder(`Latency category`, X=`Transition wait time`))) + geom_bar()+ coord_flip() + ylab("Number of OS Data Tables") + xlab("General Latency Category") + theme_bw() + scale_x_discrete(labels=c("Field Data","Domain Lab Data","External Analysis - Rolling","External Analysis - Bulk"))
+
+# alternative approach
+meanByType <- datLat %>% group_by(`Latency category`) %>% summarise(mbt=mean(`Transition wait time`))
+latType <- count(datLat, `Latency category`)
+latencyBins <- cbind(latType, meanByType$mbt)
+colnames(latencyBins) <- c("category","n","mn")
+
+# by type
 external <- dat %>% filter(Location == 'External' & `Latency category` != "OT")
 
 ggplot(external, aes(`Transition wait time`)) + stat_bin(aes(y = (..count..)/sum(..count..)*100), binwidth = 25, color = "black", fill = "#CCCCCC") + theme_bw() + scale_y_continuous("% of OS data tables") + xlab("Latency (days since field collection)")
