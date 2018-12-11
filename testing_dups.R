3a17f8495cbf9315f76225c9f43d80ae5ab7b9e0
cklunch <clunch@neoninc.org>
2018-12-11 16:05:49 -0700

    Update testing_dups.R

--- testing_dups.R
+++ testing_dups.R
@@ -11,16 +11,10 @@ data <- cfc
 source('~/GitHub/how-to-make-a-data-product/Publication workbook OS/removeDups.R')
 
 data.rem <- removeDups(data=cfc, variables=cfc.var, table='cfc_lignin_pub')
-# flagging is working on case 1 but not case 2 - ie working on resolveable dups,
-# but not on unresolveable dups. and failing to resolve when only remarks and uid differ
 
-# dplyr
-data.d <- dplyr::distinct(cfc, cfc$ligninSampleID, cfc$analyticalRepNumber, .keep_all=T)
-# doesn't treat variables as a key, treats them as the only variables to be considered
-# in determining what's a duplicate
+# testing on beetle data - can't, because this doesn't correspond to a single table
+bet.var <- 
+bet.rem <- removeDups(data=bet_all_records, variables=cfc.var, table='cfc_lignin_pub')
 
-# data.table
-data.t <- data.table(cfc, key=key)
-data.tr <- data.t[,.N,by=key(data.t)]
-# same behavior as distinct()
 
+  
