2be513c26fc906a2ef47e93cb1300e083fb6714b
cklunch <clunch@neoninc.org>
2018-12-12 15:04:27 -0700

    Update testing_dups.R

--- testing_dups.R
+++ testing_dups.R
@@ -2,7 +2,7 @@
 options(stringsAsFactors=F)
 
 cfc <- read.delim("/Users/clunch/GitHub/sandbox/cfc_lignin_dups.csv", sep=",")
-cfc.var <- read.delim("/Users/clunch/GitHub/biogeochemistryIPT/canopyFoliar/defData/cfc_datapub_L1.txt", sep="\t")
+cfc.var <- read.delim("/Users/clunch/GitHub/biogeochemistryIPT/canopyFoliar/defData/PublicationWorkbook_Plant foliar physical and chemical properties.txt", sep="\t")
 
 variables <- cfc.var
 table <- "cfc_lignin_pub"
@@ -12,9 +12,6 @@ source('~/GitHub/how-to-make-a-data-product/Publication workbook OS/removeDups.R
 
 data.rem <- removeDups(data=cfc, variables=cfc.var, table='cfc_lignin_pub')
 
-# testing on beetle data - can't, because this doesn't correspond to a single table
-bet.var <- 
-bet.rem <- removeDups(data=bet_all_records, variables=cfc.var, table='cfc_lignin_pub')
 
 
   
