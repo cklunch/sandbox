ec34bb0941d65df6334d6a8c3825a4619f2ba568
cklunch <clunch@neoninc.org>
2018-12-21 15:22:10 -0700

    Create generate_matrix.R

---
+++ data_availability/generate_matrix.R
@@ -0,0 +1,111 @@
+library(httr)
+library(jsonlite)
+library(plyr)
+
+options(stringsAsFactors=F)
+
+# get availability list
+req <- GET("http://data.neonscience.org/api/v0/products")
+ls <- fromJSON(content(req, as="text"))
+
+# get site list
+s.req <- GET("http://data.neonscience.org/api/v0/sites")
+sites <- fromJSON(content(s.req, as="text"))
+
+# set up data frame
+current <- data.frame(matrix(0, nrow=179, ncol=83))
+colnames(current)[1:2] <- c("DPID","DPName")
+colnames(current)[3:83] <- sites[[1]]$siteCode
+current$DPID <- ls[[1]]$productCode
+current$DPName <- ls[[1]]$productName
+
+# populate with 1 for availability, 0 for no
+for(i in 1:nrow(current)) {
+  dp.sites <- ls[[1]]$siteCodes[[i]]$siteCode
+  current[i,which(colnames(current) %in% dp.sites)] <- 1
+}
+
+# next up: include NAs for sites that won't ever have product
+mat <- read.delim("~/GitHub/sandbox/data_availability/Master_product_siteMatrix.csv", sep=",", skip=5)
+mat <- mat[1:180,1:85]
+
+# precip wrangling
+precip <- apply(t(mat[which(mat$Identification.Code=="NEON.DOM.SITE.DP1.00006.001"),5:85]), 
+                2, as.numeric)
+precip <- apply(precip, 1, sum)
+mat <- mat[-which(mat$Identification.Code=="NEON.DOM.SITE.DP1.00006.001"),]
+mat <- rbind(mat, c("Precipitation", "NEON.DOM.SITE.DP1.00006.001", "TIS", "", precip))
+
+for(i in colnames(mat)[5:85]) {
+  for(j in mat$Identification.Code) {
+    mat.val <- mat[which(mat$Identification.Code==j), which(colnames(mat)==i)]
+    if(mat.val > 0) {
+      next
+    } else {
+      id <- substring(j, 15, 27)
+      print(id)
+      print(i)
+      cur.val <- current[which(current$DPID==id), which(colnames(current)==i)]
+      if(length(cur.val)==0) {
+        print(paste("Data product", id, " not found"))
+      } else {
+        if(cur.val==0) {
+          current[which(current$DPID==id), which(colnames(current)==i)] <- NA
+        } else {
+          print(paste("Data available for ", id, " at ", i, " but not in matrix", sep=""))
+        }
+      }
+    }
+  }
+}
+
+# adding in 'have we ever sampled there'
+samp <- read.delim("~/GitHub/sandbox/data_availability/NEON-OS-DataAvailabilityBySiteByYear-v20170918.csv", sep=",")
+samp$DPID <- paste(substring(samp$DPID, 2, 10), ".001", sep="")
+
+
+# 1 if there are data on portal, YEAR of future sampling if it hasn't happened yet, 
+# 0 if sampling has happened but data aren't available, NA if never sampled
+for(i in current$DPID) {
+  if(length(which(samp$DPID==i))==0) {
+    print(paste("No sampling info for", i, current$DPName[which(current$DPID==i)], sep=" "))
+  } else {
+    for(j in colnames(current)[3:83]) {
+      #print(paste(i, j))
+      cur.val <- current[which(current$DPID==i), which(colnames(current)==j)]
+      if(is.na(cur.val) | cur.val==1) {
+        next
+      } else {
+        samp.dp <- samp[which(samp$DPID==i & samp$Site==j),]
+        samp.sub <- samp.dp[,grep("X", colnames(samp.dp), fixed=T)]
+        if(all(is.na(samp.sub) | samp.sub=="")) {
+          next
+        } else {
+          if(nrow(samp.sub)>1) {
+            yr <- colnames(samp.sub)[min(which(samp.sub=="Y", arr.ind=T)[,2], na.rm=T)]
+          } else {
+            yr <- colnames(samp.sub)[min(which(samp.sub=="Y"), na.rm=T)]
+          }
+          if(as.numeric(substring(yr, 2, 5)) <= 2017) {
+            next
+          } else {
+            current[which(current$DPID==i), which(colnames(current)==j)] <- substring(yr, 2, 5)
+          }
+        }
+      }
+  }
+  }
+}
+
+# remove MDP and itemized EC products
+current <- current[which(!current$DPID %in% c("DP4.50036.001","DP1.00007.001","DP1.00010.001",
+                                              "DP1.00034.001","DP1.00035.001","DP1.00036.001",
+                                              "DP1.00036.001","DP1.00037.001","DP1.00099.001",
+                                              "DP1.00100.001","DP2.00008.001","DP2.00009.001",
+                                              "DP2.00024.001","DP3.00008.001","DP3.00009.001",
+                                              "DP3.00010.001","DP4.00002.001","DP4.00007.001",
+                                              "DP4.00067.001","DP4.00137.001","DP4.00201.001")),]
+
+# write out status table
+write.table(current, "~/GitHub/sandbox/data_availability/all_status.csv", row.names=F, sep=",")
+
