40755a28a97825beaacd87c84b607590b0265118
cklunch <clunch@neoninc.org>
2019-06-07 17:35:40 -0600

    Update stackEC_exp.R

--- EC_HDF/stackEC_exp.R
+++ EC_HDF/stackEC_exp.R
@@ -15,4 +15,5 @@ flux <- stackEC(filepath="/Users/clunch/Desktop/filesToStack00200/",
 prof <- stackEC(filepath="/Users/clunch/Desktop/filesToStack00200/",
                 level="dp03", var=NA, avg=30)
 
-getVarsEC(filepath="/Users/clunch/Desktop/filesToStack00200/")
+filepath <- "/Users/clunch/Desktop/filesToStack00200/NEON.D19.BONA.DP4.00200.001.nsae.2017-12.basic.h5"
+getVarsEC(filepath="/Users/clunch/Desktop/filesToStack00200/NEON.D19.BONA.DP4.00200.001.nsae.2017-12.basic.h5")
