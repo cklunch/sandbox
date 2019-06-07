bd7e4d4d3c76eacd59c3ebda1778f07e00d03690
cklunch <clunch@neoninc.org>
2019-06-07 16:11:33 -0600

    Update stackEC_exp.R

--- EC_HDF/stackEC_exp.R
+++ EC_HDF/stackEC_exp.R
@@ -1,7 +1,18 @@
-library(neonUtilities)
-zipsByProduct(dpID="DP4.00200.001", package="basic", site="BONA",
-              savepath="/Users/clunch/Desktop", check.size=F)
-# level, var, and avg are the defaults here
-nsae <- stackEC(filepath="/Users/clunch/Desktop/filesToStack00200/",
-                    level="dp04", var=c("nsae","stor","turb"), avg=NA)
+level <- "dp04"
+filepath <- "/Users/clunch/Desktop/filesToStack00200/"
+var <- NA
+avg <- NA
 
+library(neonUtilities)
+zipsByProduct(dpID="DP4.00200.001", package="basic", 
+              site=c("BONA", "DEJU"), startdate="2017-12", enddate="2018-02",
+              savepath="/Users/clunch/Desktop", check.size=F)
+
+# if level=dp04, var and avg are ignored
+flux <- stackEC(filepath="/Users/clunch/Desktop/filesToStack00200/",
+                    level="dp04", var=NA, avg=NA)
+
+prof <- stackEC(filepath="/Users/clunch/Desktop/filesToStack00200/",
+                level="dp03", var=NA, avg=30)
+
+getVarsEC(filepath="/Users/clunch/Desktop/filesToStack00200/")
