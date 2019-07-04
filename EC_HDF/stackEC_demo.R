e07216fe2c65cb1f8865323198faaa87f8153612
cklunch <clunch@neoninc.org>
2019-07-03 18:32:56 -0600

    working on EC

--- EC_HDF/stackEC_demo.R
+++ EC_HDF/stackEC_demo.R
@@ -10,7 +10,7 @@ zipsByProduct(dpID="DP4.00200.001", package="basic",
 
 # merge the dp04 data by site
 # same syntax works for dp03 and dp02
-flux <- stackEC(filepath="/Users/clunch/Desktop/filesToStack00200/", level="dp04")
+flux <- stackEddy(filepath="/Users/clunch/Desktop/filesToStack00200/", level="dp04")
 View(flux$CPER)
 
 # for dp01, have to put in an averaging interval to use and a vector of variable names
@@ -21,7 +21,12 @@ metaEC <- getVarsEC("/Users/clunch/Desktop/filesToStack00200/NEON.D10.CPER.DP4.0
 View(metaEC)
 
 # let's get dp01 isotope data
-iso <- stackEC(filepath="/Users/clunch/Desktop/filesToStack00200/", level="dp01",
+iso <- stackEddy(filepath="/Users/clunch/Desktop/filesToStack00200/", level="dp01",
                avg=30, var=c("dlta13CCo2", "dlta18OH2o", "dlta2HH2o"))
 View(iso$BONA)
+
+# dp01 9-minute isotope data:
+raw.iso <- stackEddy(filepath="/Users/clunch/Desktop/filesToStack00200/NEON.D13.NIWO.DP4.00200.001.nsae.2018-07.basic.h5",
+                     level="dp01", var=c("rtioMoleDry12CCo2","rtioMoleDry13CCo2"), avg=9)
+
 
--- EC_HDF/stackEC_exp.R
+++ EC_HDF/stackEC_exp.R
@@ -1,7 +1,9 @@
 level <- "dp01"
-filepath <- "/Users/clunch/Desktop/filesToStack00200/"
-var <- "rtioMoleDryCo2"
-avg <- 30
+filepath <- "/Users/clunch/Desktop/filesToStack00200/NEON.D13.NIWO.DP4.00200.001.nsae.2018-07.basic.h5"
+var <- c("rtioMoleDryCo2","rtioMoleDryH2o")
+avg <- 2
+var <- NA
+avg <- NA
 
 library(neonUtilities)
 zipsByProduct(dpID="DP4.00200.001", package="basic", 
@@ -9,19 +11,22 @@ zipsByProduct(dpID="DP4.00200.001", package="basic",
               savepath="/Users/clunch/Desktop", check.size=F)
 
 # if level=dp04, var and avg are ignored
-flux <- stackEC(filepath="/Users/clunch/Desktop/filesToStack00200/",
+flux <- stackEddy(filepath="/Users/clunch/Desktop/filesToStack00200/",
                     level="dp04", var=NA, avg=NA)
 
-prof <- stackEC(filepath="/Users/clunch/Desktop/filesToStack00200/",
+prof <- stackEddy(filepath="/Users/clunch/Desktop/filesToStack00200/",
                 level="dp03", var=NA, avg=NA)
 
-temp.prof <- stackEC(filepath="/Users/clunch/Desktop/filesToStack00200/",
+temp.prof <- stackEddy(filepath="/Users/clunch/Desktop/filesToStack00200/",
                 level="dp02", var=NA, avg=NA)
 
-raw <- stackEC(filepath="/Users/clunch/Desktop/filesToStack00200/",
-                level="dp01", var=c("rtioMoleDryCo2","rtioMoleDryH2o"), avg=30)
+raw <- stackEddy(filepath="/Users/clunch/Desktop/filesToStack00200/NEON.D13.NIWO.DP4.00200.001.nsae.2018-07.basic.h5",
+                level="dp01", var=c("rtioMoleDryCo2","rtioMoleDryH2o"), avg=2)
 
-isoC <- stackEC(filepath="/Users/clunch/Desktop/filesToStack00200/",
+raw.iso <- stackEddy(filepath="/Users/clunch/Desktop/filesToStack00200/NEON.D13.NIWO.DP4.00200.001.nsae.2018-07.basic.h5",
+                 level="dp01", var=c("rtioMoleDry12CCo2","rtioMoleDry13CCo2"), avg=9)
+
+isoC <- stackEddy(filepath="/Users/clunch/Desktop/filesToStack00200/",
                level="dp01", var=c("dlta13CCo2"), avg=30)
 
 
@@ -30,7 +35,7 @@ ec.m <- getVarsEC(filepath="/Users/clunch/Desktop/filesToStack00200/NEON.D19.BON
 
 
 # from portal download
-flux <- stackEC(filepath="/Users/clunch/Desktop/NEON_eddy-flux.zip",
+flux <- stackEddy(filepath="/Users/clunch/Desktop/NEON_eddy-flux.zip",
                 level="dp04", var=NA, avg=NA)
 
 
@@ -39,16 +44,16 @@ zipsByProduct(dpID="DP4.00200.001", package="expanded",
               site=c("BONA", "DEJU"), startdate="2017-12", enddate="2018-02",
               savepath="/Users/clunch/Desktop", check.size=F)
 ex.m <- getVarsEC(filepath="/Users/clunch/Desktop/filesToStack00200/NEON.D19.BONA.DP4.00200.001.nsae.2017-12-01.expanded.h5")
-flux.ex <- stackEC(filepath="/Users/clunch/Desktop/filesToStack00200/",
+flux.ex <- stackEddy(filepath="/Users/clunch/Desktop/filesToStack00200/",
                 level="dp04", var=NA, avg=NA)
 
-prof.ex <- stackEC(filepath="/Users/clunch/Desktop/filesToStack00200/",
+prof.ex <- stackEddy(filepath="/Users/clunch/Desktop/filesToStack00200/",
                 level="dp03", var=NA, avg=NA)
 
-temp.prof.ex <- stackEC(filepath="/Users/clunch/Desktop/filesToStack00200/",
+temp.prof.ex <- stackEddy(filepath="/Users/clunch/Desktop/filesToStack00200/",
                      level="dp02", var=NA, avg=NA)
 
-isoC <- stackEC(filepath="/Users/clunch/Desktop/filesToStack00200/",
+isoC <- stackEddy(filepath="/Users/clunch/Desktop/filesToStack00200/",
                 level="dp01", var=c("dlta13CCo2"), avg=30)
 
 
