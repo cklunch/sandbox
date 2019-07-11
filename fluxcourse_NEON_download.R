3d6e241fc7c9234e69ed713c94fd93c06f3664b8
cklunch <clunch@neoninc.org>
2019-07-11 16:08:50 -0600

    Create fluxcourse_NEON_download.R

---
+++ fluxcourse_NEON_download.R
@@ -0,0 +1,17 @@
+# Install neonUtilities even if you've installed it before!
+# You'll need to have the most recent version, v1.3.0
+install.packages('neonUtilities')
+library(neonUtilities)
+
+# PLACEHOLDER! Put in a logical file path for your machine
+# This should be a place where you can put ~750 MB of data
+filepath <- '/User/Data'
+
+# This function will download the data. Don't worry about
+# how it works or what the inputs mean! I'll go over that 
+# on Friday.
+zipsByProduct(dpID='DP4.00200.001', package='basic', 
+              site=c('NIWO', 'HARV'), 
+              startdate='2018-06', enddate='2018-07',
+              savepath=filepath, 
+              check.size=F)
