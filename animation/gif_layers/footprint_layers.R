5f0fd91f584b77f83405a6ef4005152a9e3bced3
Claire Lunch <clunch@battelleecology.org>
2019-10-16 17:47:54 -0600

    Update footprint_layers.R

--- animation/gif_layers/footprint_layers.R
+++ animation/gif_layers/footprint_layers.R
@@ -4,7 +4,9 @@ options(stringsAsFactors = F)
 
 byTileAOP(dpID='DP3.30010.001', site='WREF', year='2018',
           easting=581418, northing=5074637, savepath='/Users/clunch/Desktop')
-tower.tile <- raster('/Users/clunch/Desktop/DP3.30010.001/2018/FullSite/D16/2018_WREF_2/L3/Camera/Mosaic/2018_WREF_2_581000_5074000_image.tif')
-tl <- stack(tower.tile)
-plotRGB(tl, r=3, g=2, b=1) # doesn't work
+tl <- stack('/Users/clunch/Desktop/DP3.30010.001/2018/FullSite/D16/2018_WREF_2/L3/Camera/Mosaic/2018_WREF_2_581000_5074000_image.tif')
+plotRGB(tl, r=1, g=2, b=3)
+# where's the tower?
+points(581418, 5074637, pch=19, col='white')
 
+# next step: get dimensions of footprint.
