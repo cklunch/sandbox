aacfb2d81dbd9117decf29e59a1916215686d7cd
Claire Lunch <clunch@battelleecology.org>
2019-10-31 17:13:29 -0600

    Update nu_testing.R

--- nu_testing.R
+++ nu_testing.R
@@ -9,6 +9,12 @@ pr <- loadByProduct(dpID='DP1.00024.001', site=c('WREF','ABBY'),
 cfc <- loadByProduct(dpID='DP1.10026.001', site=c('WREF','SCBI'),
              package='expanded')
 
+gwe <- loadByProduct(dpID='DP1.20100.001', site=c('MART','WLOU'),
+                    startdate='2019-07', enddate='2019-09')
+
+wch <- loadByProduct(dpID='DP1.20093.001', site=c('ARIK','POSE'),
+                     package='expanded')
+
 
 zipsByProduct(dpID='DP1.00024.001', site=c('WREF','ABBY'),
               startdate='2019-07', enddate='2019-08',
