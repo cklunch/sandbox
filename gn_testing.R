2841f33e42b8cf0d2d8935f370dc696f227f4458
Claire Lunch <clunch@battelleecology.org>
2020-02-10 17:21:12 -0700

    Update gn_testing.R

--- gn_testing.R
+++ gn_testing.R
@@ -2,6 +2,7 @@ library(devtools)
 library(neonUtilities)
 library(geoNEON)
 setwd("~/GitHub/NEON-geolocation/geoNEON")
+options(stringsAsFactors = F)
 
 bird <- loadByProduct(dpID='DP1.10003.001', site='WREF', check.size=F)
 bird$brd_perpoint <- getLocByName(bird$brd_perpoint)
