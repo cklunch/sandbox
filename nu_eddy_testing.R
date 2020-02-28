463df8ee9ea777f6ca9afbc7b4ccf556f2f0c0a8
Claire Lunch <clunch@battelleecology.org>
2020-02-28 15:55:20 -0700

    Update nu_eddy_testing.R

--- nu_eddy_testing.R
+++ nu_eddy_testing.R
@@ -1,6 +1,18 @@
 library(neonUtilities)
-library(raster)
-ras <- footRaster('/Users/clunch/Desktop/filesToStack00200/NEON.D06.KONZ.DP4.00200.001.2018-01.expanded.20190521T201855Z.zip')
+library(devtools)
+setwd('/Users/clunch/GitHub/NEON-utilities/neonUtilities/')
+install('.')
 
+flux <- stackEddy('/Users/clunch/Desktop/filesToStack00200KONZ/', level='dp04')
+iso <- stackEddy('/Users/clunch/Desktop/filesToStack00200/', level='dp01', 
+                 var=c('rtioMoleDryCo2','dlta13CCo2'), avg=30)
 
+ras <- footRaster('/Users/clunch/Desktop/filesToStack00200KONZ/NEON.D06.KONZ.DP4.00200.001.nsae.2018-04-19.expanded.h5')
+ras <- footRaster('/Users/clunch/Desktop/KONZmini/')
+raster::plot(ras$KONZ.summary)
 
+zipsByProduct(dpID='DP4.00200.001', site='WREF', startdate='2018-07', enddate='2018-08',
+              savepath='/Users/clunch/Desktop')
+flux <- stackEddy('/Users/clunch/Desktop/filesToStack00200/', level='dp04')
+iso <- stackEddy('/Users/clunch/Desktop/NEON_eddy-flux.zip', level='dp01', 
+                 var=c('rtioMoleDryCo2','dlta13CCo2'), avg=30)
