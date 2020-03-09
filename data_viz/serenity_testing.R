59f2701b2eb0f4390ebb3ee4416bc026db863610
Claire Lunch <clunch@battelleecology.org>
2020-03-09 10:19:03 -0600

    Update serenity_testing.R

--- data_viz/serenity_testing.R
+++ data_viz/serenity_testing.R
@@ -7,7 +7,10 @@ options(stringsAsFactors=F)
 # get canopy chemistry data
 cfc <- loadByProduct(dpID="DP1.10026.001", check.size=F)
 
-# join tables
+# open one table in serenity
+serenity.viz(dataset=cfc$cfc_carbonNitrogen)
+
+# to enable more meaningful comparisons, need to join tables
 leafchem <- merge(cfc$cfc_fieldData, cfc$cfc_carbonNitrogen, 
                   by=c("namedLocation","domainID","siteID","plotID",
                        "plotType","collectDate","sampleID","sampleCode"), 
@@ -17,7 +20,7 @@ leafchem <- merge(leafchem, cfc$cfc_elements,
                        "plotType","collectDate","sampleID","sampleCode"),
                   all=T)
 
-# subset to reasonable variables and send to serenity
+# subset to some reasonable variables and send to serenity
 leafsub <- leafchem[,c("siteID","plotType","nlcdClass","taxonID",
                        "nitrogenPercent","carbonPercent","CNratio",
                        "dryMass","foliarPhosphorusConc","foliarPotassiumConc",
