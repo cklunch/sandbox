4ac573b1dd0fbe6f9cf8ed9edc7b9f7f3c41681b
Claire Lunch <clunch@battelleecology.org>
2020-03-10 15:19:16 -0600

    Update nu_eddy_testing.R

--- nu_eddy_testing.R
+++ nu_eddy_testing.R
@@ -12,7 +12,20 @@ ras <- footRaster('/Users/clunch/Desktop/KONZmini/')
 raster::plot(ras$KONZ.summary)
 
 zipsByProduct(dpID='DP4.00200.001', site='WREF', startdate='2018-07', enddate='2018-08',
-              savepath='/Users/clunch/Desktop')
+              package='expanded', savepath='/Users/clunch/Desktop')
 flux <- stackEddy('/Users/clunch/Desktop/filesToStack00200/', level='dp04')
 iso <- stackEddy('/Users/clunch/Desktop/NEON_eddy-flux.zip', level='dp01', 
                  var=c('rtioMoleDryCo2','dlta13CCo2'), avg=30)
+
+ras <- footRaster('/Users/clunch/Desktop/filesToStack00200WREF/')
+raster::plot(ras$WREF.summary)
+raster::filledContour(ras$WREF.summary, col=topo.colors(24),
+                      xlim=c(580000,582500), ylim=c(5073500,5076000))
+
+raster::filledContour(ras$NEON.D16.WREF.DP4.00200.001.nsae.2018.07.01.expanded.WREF.dp04.data.foot.grid.turb.20180701T110000Z, col=topo.colors(24),
+                      xlim=c(580000,582500), ylim=c(5073500,5076000))
+
+raster::filledContour(ras$NEON.D16.WREF.DP4.00200.001.nsae.2018.07.01.expanded.WREF.dp04.data.foot.grid.turb.20180701T143000Z, 
+                      col=topo.colors(24),
+                      xlim=c(580000,582500), ylim=c(5073500,5076000))
+
