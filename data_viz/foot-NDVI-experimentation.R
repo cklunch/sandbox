ba3b89e26e8af0e7710c04f879c88799242da67f
Claire Lunch <clunch@battelleecology.org>
2022-07-15 17:51:02 -0600

    Update foot-NDVI-experimentation.R

--- data_viz/foot-NDVI-experimentation.R
+++ data_viz/foot-NDVI-experimentation.R
@@ -59,7 +59,7 @@ plot(flux$CLBJ$data.fluxCo2.turb.flux~flux$CLBJ$timeEnd, pch=20)
 fluxpath <- '/Users/clunch/Desktop/filesToStack00200/'
 aoppath <- '/Users/clunch/Desktop/DP3.30026.001/'
 flight.dates <- read.csv('/Users/clunch/Downloads/FlightDatesThrough2021.csv')
-site.dates <- flight.dates[grep('NOGP', flight.dates$YearSiteVisit),]
+site.dates <- flight.dates[grep('CLBJ', flight.dates$YearSiteVisit),]
 ex.dates <- substring(site.dates$FlightDate, 1, 8)
 hy.dates <- paste(substring(ex.dates, 1, 4), substring(ex.dates, 5, 6),
                   substring(ex.dates, 7, 8), sep='-')
@@ -70,9 +70,9 @@ comp <- matrix(data=NA, nrow=0, ncol=4)
 comp <- data.frame(comp)
 names(comp) <- c('sm','nee','num.mis.nee','ndvi')
 
-for(i in 4:length(months)) {
+for(i in 3:length(months)) {
 
-  zipsByProduct(dpID='DP4.00200.001', site='NOGP', startdate=months[i], enddate=months[i],
+  zipsByProduct(dpID='DP4.00200.001', site='CLBJ', startdate=months[i], enddate=months[i],
                 package='expanded', check.size=F, #release='RELEASE-2022', 
                 savepath='/Users/clunch/Desktop', token=Sys.getenv('NEON_TOKEN'))
   
@@ -80,31 +80,31 @@ for(i in 4:length(months)) {
   lapply(fl, utils::unzip, exdir=fluxpath)
   fls <- list.files(fluxpath, pattern=paste(hy.dates[grep(months[i], hy.dates)], collapse="|"), 
                     full.names=T, recursive=T)
-  fls <- grep('NOGP', fls, value=T)
+  fls <- grep('CLBJ', fls, value=T)
   lapply(fls, R.utils::gunzip, overwrite=T, remove=T)
   fls <- list.files(fluxpath, pattern='[.]h5$', full.names=T, recursive=T)
-  fls <- grep('NOGP', fls, value=T)
+  fls <- grep('CLBJ', fls, value=T)
   fls <- grep(months[i], fls, value=T)
   
   ftStack <- footRaster(fls)
-  foot <- ftStack$NOGP.summary
+  foot <- ftStack$CLBJ.summary
   
   flux <- stackEddy(fls, level='dp04')
-  try(plot(flux$NOGP$data.fluxCo2.nsae.flux~flux$NOGP$timeEnd, pch=20))
+  try(plot(flux$CLBJ$data.fluxCo2.nsae.flux~flux$CLBJ$timeEnd, pch=20))
 
   wid <- extent(foot)[2] - extent(foot)[1]
   num <- floor(wid/1000)
   xcoord <- c(extent(foot)[1], extent(foot)[1]+I(1000*1:num), extent(foot)[2])
   ycoord <- c(extent(foot)[3], extent(foot)[3]+I(1000*1:num), extent(foot)[4])
   
-  byTileAOP(dpID='DP3.30026.001', site='NOGP', year=substring(months[i], 1, 4),
+  byTileAOP(dpID='DP3.30026.001', site='CLBJ', year=substring(months[i], 1, 4),
             easting=expand.grid(xcoord, ycoord)$Var1,
             northing=expand.grid(xcoord, ycoord)$Var2, 
             check.size=F, savepath='/Users/clunch/Desktop', token=Sys.getenv('NEON_TOKEN'))
-  afls <- list.files(aoppath, pattern='NOGP(.*)zip$', full.names=T, recursive=T)
+  afls <- list.files(aoppath, pattern='CLBJ(.*)zip$', full.names=T, recursive=T)
   afls <- grep(substring(months[i], 1, 4), afls, value=T)
   lapply(afls, utils::unzip, exdir=dirname(afls[1]))
-  afls <- list.files(aoppath, pattern='NOGP(.*)_NDVI.tif$', full.names=T, recursive=T)
+  afls <- list.files(aoppath, pattern='CLBJ(.*)_NDVI.tif$', full.names=T, recursive=T)
   
   ndvilist <- list()
   for(j in 1:length(afls)) {
@@ -123,9 +123,9 @@ for(i in 4:length(months)) {
   comb <- foot.ndvi*ndvi.foot
   w.ndvi <- cellStats(comb, stat='sum', na.rm=T)
   
-  comp <- rbind(comp, c(months[i], sum(flux$NOGP$data.fluxCo2.nsae.flux, na.rm=T),
-                        length(which(is.na(flux$NOGP$data.fluxCo2.nsae.flux) | 
-                                       flux$NOGP$data.fluxCo2.nsae.flux=='NaN')),
+  comp <- rbind(comp, c(months[i], sum(flux$CLBJ$data.fluxCo2.nsae.flux, na.rm=T),
+                        length(which(is.na(flux$CLBJ$data.fluxCo2.nsae.flux) | 
+                                       flux$CLBJ$data.fluxCo2.nsae.flux=='NaN')),
                         w.ndvi))
 
 }
@@ -136,7 +136,7 @@ for(i in 4:length(months)) {
 fluxpath <- '/Users/clunch/Desktop/filesToStack00200/'
 aoppath <- '/Users/clunch/Desktop/DP3.30026.001/'
 flight.dates <- read.csv('/Users/clunch/Downloads/FlightDatesThrough2021.csv')
-site.dates <- flight.dates[grep('CPER|OAES|NOGP|WOOD', flight.dates$YearSiteVisit),]
+site.dates <- flight.dates[grep('KONZ', flight.dates$YearSiteVisit),]
 ex.dates <- substring(site.dates$FlightDate, 1, 8)
 hy.dates <- paste(substring(ex.dates, 1, 4), substring(ex.dates, 5, 6),
                   substring(ex.dates, 7, 8), sep='-')
@@ -284,14 +284,60 @@ ndvi.w <- data.frame(ndvi.w)
 names(ndvi.w) <- c('site','month','ndvi')
 ndvi.w$ndvi <- as.numeric(ndvi.w$ndvi)
 
-f.all$timeBgn <- as.POSIXct(f.all$timeBgn, tz='GMT', format='%Y-%m-%d %H:%M:%S')
+write.table(ndvi.w, 
+            '/Users/clunch/Library/CloudStorage/Box-Box/NEON_Lunch/data/fluxcourse_data/ndvi_set.csv',
+            sep=',', row.names=F)
+ndvi.w <- read.csv('/Users/clunch/Library/CloudStorage/Box-Box/NEON_Lunch/data/fluxcourse_data/ndvi_set.csv')
 
-site <- 'NOGP'
-startd <- as.POSIXct('2021-06-14', tz='GMT')
-endd <- as.POSIXct('2021-06-16', tz='GMT')
+f.all$timeBgn <- as.POSIXct(f.all$timeBgn, tz='GMT', format='%Y-%m-%d %H:%M:%S')
+f.all$timeEnd <- as.POSIXct(f.all$timeEnd, tz='GMT', format='%Y-%m-%d %H:%M:%S')
+
+site <- 'WOOD'
+startd <- as.POSIXct('2021-06-10', tz='GMT')
+endd <- as.POSIXct('2021-06-15', tz='GMT')
 
 f.sub <- f.all[which(f.all$siteID==site & 
                        f.all$timeBgn>=startd &
                        f.all$timeBgn<endd),]
 
 plot(f.sub$data.fluxCo2.nsae.flux~f.sub$timeBgn, pch=20)
+
+
+# compare flux to ndvi
+fl.dt <- read.csv('/Users/clunch/Library/CloudStorage/Box-Box/NEON_Lunch/data/fluxcourse_data/flight_dates.csv')
+fl.dt$FlightDate <- as.POSIXct(fl.dt$FlightDate, tz='GMT')
+
+ndvi.f <- ndvi.w[ndvi.w$site %in% fl.dt$Site,]
+ndvi.f <- ndvi.f[ndvi.f$month %in% substring(as.character(fl.dt$FlightDate), 1, 7),]
+ndvi.f$flux <- rep(NA, nrow(ndvi.f))
+ndvi.f$dayflux <- rep(NA, nrow(ndvi.f))
+for(i in 1:nrow(fl.dt)) {
+  
+  f.sub <- f.all[which(f.all$siteID==fl.dt$Site[i] & 
+                         f.all$timeBgn>=I(fl.dt$FlightDate[i]-86400) &
+                         f.all$timeBgn<I(fl.dt$FlightDate[i]+86400)),]
+  fl <- sum(f.sub$data.fluxCo2.nsae.flux, na.rm=T)/3
+  fl.d <- sum(f.sub$data.fluxCo2.nsae.flux[which(f.sub$data.fluxCo2.nsae.flux<0)], na.rm=T)/3
+  ndvi.f$flux[which(ndvi.f$site==fl.dt$Site[i] & ndvi.f$month==
+                      substring(as.character(fl.dt$FlightDate[i]), 1, 7))] <- fl
+  ndvi.f$dayflux[which(ndvi.f$site==fl.dt$Site[i] & ndvi.f$month==
+                      substring(as.character(fl.dt$FlightDate[i]), 1, 7))] <- fl.d
+  
+}
+
+ndvi.f <- ndvi.f[-which(is.na(ndvi.f$flux)),]
+
+plot(ndvi.f$flux~ndvi.f$ndvi, pch=20, xlab='NDVI', ylab='NEE')
+plot(ndvi.f$dayflux~ndvi.f$ndvi, pch=20, xlab='NDVI', ylab='NEE')
+
+plot(ndvi.f$flux~ndvi.f$ndvi, pch=20, xlab='NDVI', ylab='NEE', type='n')
+text(ndvi.f$ndvi, ndvi.f$flux, labels=ndvi.f$site, cex=0.5)
+
+plot(ndvi.f$dayflux~ndvi.f$ndvi, pch=20, xlab='NDVI', ylab='NEE', type='n')
+text(ndvi.f$ndvi, ndvi.f$dayflux, labels=ndvi.f$site, cex=0.5)
+
+
+# next steps:
+# 1. rename WOOD to DCFS
+# 2. subset to needed files
+# 3. write up (!)
