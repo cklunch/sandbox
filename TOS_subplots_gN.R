750c84b13a2d2e832b3a06e58b5c3ba8151d28d2
Claire Lunch <clunch@neoninc.org>
2024-02-22 11:54:12 -0700

    Update TOS_subplots_gN.R

--- TOS_subplots_gN.R
+++ TOS_subplots_gN.R
@@ -6,28 +6,69 @@ library(geoNEON)
 # table of DPIDs and table names to be checked
 dps <- read.csv('/Users/clunch/GitHub/sandbox/TOS_subplot_prods.csv')
 
-l23 <- loadByProduct('DP1.10033.001', 
-                     site='HARV', 
-                     release='RELEASE-2023',
-                     startdate='2021-01', 
-                     enddate='2021-12',
-                     check.size=F,
-                     token=Sys.getenv('NEON_TOKEN'))
+for(i in c(4)) {
+  
+  d23 <- loadByProduct(dps$dpID[i], 
+                       site='JERC', 
+                       release='RELEASE-2023',
+                       startdate='2018-01', 
+                       enddate='2019-12',
+                       check.size=F,
+                       token=Sys.getenv('NEON_TOKEN'))
+  
+  d24 <- loadByProduct(dps$dpID[i], 
+                       site='JERC', 
+                       release='RELEASE-2024',
+                       startdate='2018-01', 
+                       enddate='2019-12',
+                       check.size=F,
+                       token=Sys.getenv('NEON_TOKEN'))
+  
+  g23 <- getLocTOS(d23[[dps$tableName[i]]], 
+                   dps$tableName[i],
+                   token=Sys.getenv('NEON_TOKEN'))
+  
+  g24 <- getLocTOS(d24[[dps$tableName[i]]], 
+                   dps$tableName[i],
+                   token=Sys.getenv('NEON_TOKEN'))
+  
+  print(dps$tableName[i])
+  print(all(g23$adjEasting==g24$adjEasting))
+  
+}
 
-l24 <- loadByProduct('DP1.10033.001', 
-                     site='HARV', 
-                     release='RELEASE-2024',
-                     startdate='2021-01', 
-                     enddate='2021-12',
-                     check.size=F,
-                     token=Sys.getenv('NEON_TOKEN'))
+# vst comparison
+for(j in 1:nrow(g24)) {
+  if(!is.na(g24$individualID[j])) {
+    if(!is.na(g24$pointID[j])) {
+      if(!is.na(g24$stemDistance[j])) {
+        if(g24$individualID[j] %in% g23$individualID & 
+           g24$eventID[j] %in% g23$eventID) {
+          if(g24$adjEasting[j]!=g23$adjEasting[which(g23$individualID==g24$individualID[j] &
+                                                     g23$eventID==g24$eventID[j])]) {
+            print(j)
+          }
+        }
+      }
+    }
+  }
+}
 
-g23 <- getLocTOS(l23$ltr_pertrap, 
-                 'ltr_pertrap',
-                 token=Sys.getenv('NEON_TOKEN'))
+# sls comparison
+for(k in 1:nrow(g24)) {
+  if(g24$adjEasting[k]!=g23$adjEasting[which(g23$sampleID==g24$sampleID[k])]) {
+    print(k)
+  }
+}
 
-g24 <- getLocTOS(l24$ltr_pertrap, 
-                 'ltr_pertrap',
-                 token=Sys.getenv('NEON_TOKEN'))
+# div comparison
+for(k in 1:nrow(g24)) {
+  if(g24$adjEasting[k]!=g23$adjEasting[which(g23$subplotID==g24$subplotID[k] &
+                                             g23$eventID==g24$eventID[k] &
+                                             g23$taxonID==g24$taxonID[k])]) {
+    print(k)
+  }
+}
+
 
 
