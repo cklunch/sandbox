e9a80915eca4873693db76bd1bbab341942179df
Claire Lunch <clunch@neoninc.org>
2025-07-30 09:05:25 -0600

    Update div_BLAN_ex.R

--- MEFA/div_BLAN_ex.R
+++ MEFA/div_BLAN_ex.R
@@ -5,12 +5,18 @@ library(geoNEON)
 
 # if you're not familiar with using a token, go here: https://www.neonscience.org/resources/learning-hub/tutorials/neon-api-tokens-tutorial
 # or leave out the token, but including it will make this faster
+
+# get plant presence and percent cover data from BLAN
 div <- loadByProduct('DP1.10058.001', site='BLAN', 
                      startdate='2023-01', package='expanded',
                      include.provisional=F, check.size=F,
                      token=Sys.getenv('NEON_TOKEN'))
+
+# get subplot locations
 div1m2loc <- getLocTOS(div$div_1m2Data, dataProd='div_1m2Data',
                        token=Sys.getenv('NEON_TOKEN'))
+
+# convert subplot locations from UTM 18N to 17N
 div1m2loc17 <- convertBLAN(div1m2loc, 
                            easting="adjEasting",
                            northing="adjNorthing")
